801de9f7a52029205fd4cc6166a50778a3e23bc3
Susana Murillo <susana.murillo@uabc.edu.mx>
2021-05-15 15:57:59 -0700

    views updates

---
+++ app/src/main/java/com/example/buzonfca/SplashPantalla.kt
@@ -0,0 +1,11 @@
+package com.example.buzonfca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashPantalla : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_pantalla)
+    }
+}
