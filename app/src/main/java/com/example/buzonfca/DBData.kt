a4df632ec18b31bf53ef4697e395310b05913b3a
Susana Murillo <susana.murillo@uabc.edu.mx>
2021-05-15 21:27:17 -0700

    database data view

---
+++ app/src/main/java/com/example/buzonfca/DBData.kt
@@ -0,0 +1,4 @@
+package com.example.buzonfca
+
+class DBData {
+}
---
+++ app/src/main/java/com/example/buzonfca/DataView.kt
@@ -0,0 +1,11 @@
+package com.example.buzonfca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_view)
+    }
+}
